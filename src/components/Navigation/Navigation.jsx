57746a61b0a31d2a9771f4f8baa6f13dc25e0597
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-06-19 23:16:14 +0200

    1

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,14 @@
 import css from "./Navigation.module.css";
 
 const Navigation = () => {
-  return <div className={css.nav}>Navigation</div>;
+  return (
+    <div className={css.nav}>
+      <ul className={css.list}>
+        <li className={css.item}>Home</li>
+        <li className={css.item}>Movies</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navigation;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,18 @@
 import css from "./HomePage.module.css";
+import Navigation from "../../components/Navigation/Navigation";
 
 const HomePage = ({ trendMovie }) => {
   return (
-    <div className={css.container}>
-      <h1>Trending today</h1>
-      <ul>
-        {trendMovie.length > 0 &&
-          trendMovie.map((movie) => <li key={movie.id}>{movie.title}</li>)}
-      </ul>
-    </div>
+    <>
+      <Navigation />
+      <div className={css.container}>
+        <h1>Trending today</h1>
+        <ul>
+          {trendMovie.length > 0 &&
+            trendMovie.map((movie) => <li key={movie.id}>{movie.title}</li>)}
+        </ul>
+      </div>
+    </>
   );
 };
 
