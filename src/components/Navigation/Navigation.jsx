f88d5fd8c1b414cd76f6d07c1970caae773d9868
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-06-20 11:53:55 +0200

    1

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,22 @@
+import { NavLink } from "react-router-dom";
 import css from "./Navigation.module.css";
+import clsx from "clsx";
+
+const getClassNames = ({ isActive }) => {
+  return clsx(css.link, isActive && css.isActive);
+};
 
 const Navigation = () => {
   return (
-    <div className={css.nav}>
-      <ul className={css.list}>
-        <li className={css.item}>Home</li>
-        <li className={css.item}>Movies</li>
-      </ul>
+    <div className={css.container}>
+      <nav className={css.nav}>
+        <NavLink className={getClassNames} to="/">
+          Home
+        </NavLink>
+        <NavLink className={getClassNames} to="/movies">
+          Movies
+        </NavLink>
+      </nav>
     </div>
   );
 };
