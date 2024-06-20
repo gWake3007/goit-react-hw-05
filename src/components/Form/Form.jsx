98e61939ef9f12b5676900a942891923c153f28b
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-06-20 12:23:33 +0200

    1

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,11 +3,15 @@ import css from "./Form.module.css";
 const Form = () => {
   return (
     <form className={css.form}>
-      <label htmlFor="searchInput">
-        Please enter a title to search for a movie
-      </label>
-      <input type="text" id="searchInput" />
-      <button type="submit">Search</button>
+      <div className={css.container}>
+        <label className={css.label} htmlFor="searchInput">
+          Please enter a title to search for a movie
+        </label>
+        <input className={css.input} type="text" id="searchInput" />
+      </div>
+      <button className={css.btn} type="submit">
+        Search
+      </button>
     </form>
   );
 };
